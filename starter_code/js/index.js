e5af4dba984764ca7e4b2e48f17cc49008bea174
talhakhan1 <talhak65@gmail.com>
2017-03-26 20:44:19 -0700

    assignment

---
+++ starter_code/js/index.js
@@ -0,0 +1,15 @@
+
+// $(document).ready(function(){
+// 	console.log('ready');
+// })
+
+//listen to the click on update
+
+$('#submit-btn').click(austin);
+	function austin(){
+		$('body').css('background-image','src="images/austin.jpg"')
+		event.prevent default();
+
+		
+	}
+
