f0b7baac4d04a3e8774973ce5bdf73c9650158bf
talhakhan1 <talhak65@gmail.com>
2017-03-26 22:34:02 -0700

    need some help

--- starter_code/js/index.js
+++ starter_code/js/index.js
@@ -5,11 +5,27 @@
 
 //listen to the click on update
 
-$('#submit-btn').click(austin);
-	function austin(){
-		$('body').css('background-image','src="images/austin.jpg"')
-		event.prevent default();
+// $('#submit-btn').click(img);
+// 	function img(){
+// 		$('body').css('background-image','url(images/austin.jpg)');
+// 		event.preventDefault();
+		
+	// }
+
+var bkgimg;
+var cityname;
+	cityname = $('#city-type');
+	bkgimg = $('url (images/citipix_skyline.jpg)');
+
+	if(cityname === 'nyc'){
+
+
+
+
+			bkgimg = "('body').css('background-image','url(images/nyc.jpg)')";
+			
 
 		
-	}
+}
+
 
