5ad0527f900baacb4540f3983bd39c1dc48f142f
talhakhan1 <talhak65@gmail.com>
2017-03-28 17:16:02 -0700

    Assignment completed

--- starter_code/js/index.js
+++ starter_code/js/index.js
@@ -5,27 +5,54 @@
 
 //listen to the click on update
 
-// $('#submit-btn').click(img);
-// 	function img(){
-// 		$('body').css('background-image','url(images/austin.jpg)');
-// 		event.preventDefault();
+$('#submit-btn').click(img);
+
+function img(){
+	//Get input value
+	var cityname;
+		cityname = $('#city-type').val().toLowerCase().trim();
+	
+	// var bkgimg = $('url (images/citipix_skyline.jpg)');
+	//If input value equals New York City
+	if (cityname === 'nyc' || cityname === 'new york city' || cityname === 'new york')
+		//change background-image to nyc.jpg
+		$('body').css('background-image','url(images/nyc.jpg)');
+	
+	
+
+	// if input value equals San-francisco 
+	else if (cityname === 'san francisco' || cityname === 'sf' || cityname === 'bay area')
+		// change background image to sf.jpg
+
+		$('body').css('background-image' , 'url(images/sf.jpg)');
+
+
+	// if input value equals LA
+	else if (cityname === 'la' || cityname === 'los angeles' || cityname === 'lax')
+		//  change background inage to LA.jpg
+		$('body').css('background-image' , 'url(images/la.jpg)');
+	
+	// if input value equals austin	
+	else if	(cityname === 'austin' || cityname === 'atx')
+		// change background image to austin.jpg
+		$('body').css('background-image' , 'url(images/austin.jpg)');
+	
+	// if input value equals sydney
+
+	else if (cityname === 'sydney' || cityname === 'syd')
+		// change background image to  sydney.jpg
+		$('body').css('background-image', 'url(images/sydney.jpg)');
 		
-	// }
-
-var bkgimg;
-var cityname;
-	cityname = $('#city-type');
-	bkgimg = $('url (images/citipix_skyline.jpg)');
-
-	if(cityname === 'nyc'){
+	
+	// if input value equals null
+	else $('body').css('background-image' , 'url(images/citipix_skyline.jpg)');
 
 
+	event.preventDefault();
+	
 
-
-			bkgimg = "('body').css('background-image','url(images/nyc.jpg)')";
-			
-
-		
 }
+
+
 
 
