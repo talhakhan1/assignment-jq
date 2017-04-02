f5d07f5c46bd8c5331620bf62af97d204cea8428
talhakhan1 <talhak65@gmail.com>
2017-04-02 15:30:27 -0700

    Assignment completed

--- starter_code/js/index.js
+++ starter_code/js/index.js
@@ -2,50 +2,52 @@
 // $(document).ready(function(){
 // 	console.log('ready');
 // })
+// create dropdown selectors
+var cityname=["San Francisco" , "Los Angeles" , "New York City"  , "Austin"  , "Sydney"];
+// Add options in the selector
+cityname.forEach(function(city){
+	console.log(city);
+	var cityOption= '<option value="' + city + '">' + city + '</option>';
+	$('.dropdown').append(cityOption);
+})
 
 //listen to the click on update
 
-$('#submit-btn').click(img);
+$('.dropdown').change(img);
 
 function img(){
 	//Get input value
-	var cityname;
-		cityname = $('#city-type').val().toLowerCase().trim();
+	// var cityname;
+	var cityVal = $('.dropdown').val();
+	console.log(cityVal); 
 	
-	// var bkgimg = $('url (images/citipix_skyline.jpg)');
-	//If input value equals New York City
-	if (cityname === 'nyc' || cityname === 'new york city' || cityname === 'new york')
-		//change background-image to nyc.jpg
-		$('body').css('background-image','url(images/nyc.jpg)');
-	
-	
-
-	// if input value equals San-francisco 
-	else if (cityname === 'san francisco' || cityname === 'sf' || cityname === 'bay area')
-		// change background image to sf.jpg
-
-		$('body').css('background-image' , 'url(images/sf.jpg)');
-
-
+	//If input value equals San francisco
+	if(cityVal === 'San Francisco')
+		// add class to body
+		$('body').attr('class' , 'sf');
+	// If input value is New york City
+	else if (cityVal === 'New York City')
+		//add class to body
+		$('body').attr('class' , 'nyc');
+	 
 	// if input value equals LA
-	else if (cityname === 'la' || cityname === 'los angeles' || cityname === 'lax')
-		//  change background inage to LA.jpg
-		$('body').css('background-image' , 'url(images/la.jpg)');
+	else if (cityVal ==='Los Angeles')
+		//  Add class to body 
+		$('body').attr('class' , 'la');
 	
 	// if input value equals austin	
-	else if	(cityname === 'austin' || cityname === 'atx')
-		// change background image to austin.jpg
-		$('body').css('background-image' , 'url(images/austin.jpg)');
-	
+	else if	(cityVal === 'Austin')
+		// Add class to body 
+		$('body').attr('class' , 'austin');
 	// if input value equals sydney
 
-	else if (cityname === 'sydney' || cityname === 'syd')
+	else if (cityVal === 'Sydney')
 		// change background image to  sydney.jpg
-		$('body').css('background-image', 'url(images/sydney.jpg)');
+		$('body').attr('class' , 'sydney');
 		
 	
 	// if input value equals null
-	else $('body').css('background-image' , 'url(images/citipix_skyline.jpg)');
+	// else $('body').css('background-image' , 'url(images/citipix_skyline.jpg)');
 
 
 	event.preventDefault();
